e1e8c76b9e9eb3805c5153dc4139a1b6565be995
Kevin <kevinchiduku16@gmail.com>
2026-05-08 13:17:54 +0100

    updated gallery and index

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import random
 from flask import Flask, render_template
 from data import BIO, PROJECTS, SKILLS
@@ -6,17 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Home always shows the top 6 in order
+    # Show top 6 on Home
     return render_template('index.html', bio=BIO, projects=PROJECTS[:6], skills=SKILLS)
 
 @app.route('/archive')
 def archive():
-    # Jumbles the order for the Full Works page every time it loads
-    jumbled_projects = list(PROJECTS)
-    random.shuffle(jumbled_projects)
-    return render_template('gallery.html', bio=BIO, projects=jumbled_projects)
+    # Shuffle for the Archive page
+    jumbled = list(PROJECTS)
+    random.shuffle(jumbled)
+    return render_template('gallery.html', bio=BIO, projects=jumbled)
 
 if __name__ == '__main__':
-    import os
+    # This line allows Render to set the port, but uses 5001 on your Mac
     port = int(os.environ.get("PORT", 5001))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
